63b31ab659995151c2b0902817b42348ac1e1e9e
ctaicher <cltaicher@gmail.com>
2024-06-15 13:11:16 -0700

    Initial Commit

---
+++ diaBeatIt/diaBeatItApp.swift
@@ -0,0 +1,17 @@
+//
+//  diaBeatItApp.swift
+//  diaBeatIt
+//
+//  Created by Christopher Taicher on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct diaBeatItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
